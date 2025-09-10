bb0b966349f5efc03821d32038837d522e4c3f39
CRISTIANO DE GODOI FRANCISCANO <cristiano.godoi10@hotmail.com>
2025-09-09 21:42:08 -0300

    Add files via upload
    python-basico/
    │
    ├── 01_variaveis.py
    ├── 02_cadastro_funcionario.py
    ├── 03_if_else.py
    ├── 04_operadores.py
    ├── 05_strings.py
    └── README.md

---
+++ Mais condições-elif.py
@@ -0,0 +1,10 @@
+idade = 18
+
+if idade < 18:
+    print("Menor idade")
+elif idade == 18:
+    print("Tem exatamente 18 anos")
+else:
+    print("Maior idade")
+
+    #No Python, a indentação é lei cada nível precisa estar alinhado#
