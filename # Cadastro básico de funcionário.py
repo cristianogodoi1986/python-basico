a817bf909c72d78e5b4da2ea8f08c37684905571
CRISTIANO DE GODOI FRANCISCANO <cristiano.godoi10@hotmail.com>
2025-09-09 21:13:51 -0300

    Add files via upload

---
+++ # Cadastro básico de funcionário.py
@@ -0,0 +1,17 @@
+# Cadastro básico de funcionário
+
+# Variáveis principais
+codigo = 10         # Código identificador
+salario = 1500.00   # Salário atual do funcionário
+nome = 'Jose'       # Nome do funcionário
+situacao = True     # Situação: True = Ativo, False = Inativo
+
+# Exibição formatada dos dados
+print(f"Código: {codigo} | Nome: {nome} | O salário atual é de R$ {salario:.2f}")
+
+# Definindo as variáveis do funcionário
+codigo = 10
+salario = 1500.00
+nome = 'Jose'
+situacao = True  # Booleano: True = ativo, False = inativo
+
